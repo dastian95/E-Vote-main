4a41d910b913e55877e02ca1fc8fe02efaa682fe
dastian95 <aufadzwicaksono@gmail.com>
2025-08-22 20:04:24 +0700

    Delete E-Vote-main directory

--- E-Vote-main/E-Vote-main/assets/conn.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$conn = new mysqli("localhost","root","","voting_db");  //localhost ,username,password,databaseName
-
-     if($conn->connect_error){ //check if connection success or not
-         die("$conn->$Connect_error");
-     }
